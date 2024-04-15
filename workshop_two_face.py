dc2d64f707beca00168b91ecb35ba993123abff8
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-04-16 00:18:52 +0900

    가독성 업데이트

--- workshop_two_face.py
+++ workshop_two_face.py
@@ -13,25 +13,24 @@ img_comp = np.zeros_like(img_face)
 
 # 연산에 필요한 좌표 계산
 height, width = img_face.shape[:2]
-middle = width//2                             # 영상의 중앙 좌표
-alpha_width = width * alpha_width_rate // 100 # 알파 블렌딩 범위
-start = middle - alpha_width//2               # 알파 블렌딩 시작 지점
-step = 100/alpha_width                        # 알파 값 간격
+middle = width // 2                             # 영상의 중앙 좌표
+alpha_width = width * alpha_width_rate // 100   # 알파 블렌딩 범위
+start = middle - alpha_width // 2               # 알파 블렌딩 시작 지점
+step = 100 / alpha_width                        # 알파 값 간격
 
 # 입력 영상의 절반씩 복사해서 결과 영상에 합성
-img_comp[:, :middle, : ] = img_face[:, :middle, :].copy()
+img_comp[:, :middle, :] = img_face[:, :middle, :].copy()
 img_comp[:, middle:, :] = img_skull[:, middle:, :].copy()
 cv2.imshow('half', img_comp)
 
 # 알파 값을 바꾸면서 알파 블렌딩 적용
-for i in range(alpha_width+1 ):
+for i in range(alpha_width + 1):
     alpha = (100 - step * i) / 100  # 증감 간격에 따른 알파 값 (1~0)
     beta = 1 - alpha                # 베타 값 (0~1)
     # 알파 블렌딩 적용
-    img_comp[:, start+i] = img_face[:, start+i] * \
-                                alpha + img_skull[:, start+i] * beta
+    img_comp[:, start + i] = img_face[:, start + i] * alpha + img_skull[:, start + i] * beta
     print(i, alpha, beta)
-    
+
 cv2.imshow('half skull', img_comp)
 cv2.waitKey()
 cv2.destroyAllWindows()
